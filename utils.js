7ba7e1f2f8f6c791475072970368ea6958850532
Dan <89908678+GhostDolphin@users.noreply.github.com>
2023-01-14 02:43:42 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,45 +1,58 @@
 const fs = require('fs');
 
 const fileProcessor = {
-  viewFile: (partialsNotReady, partialsReady) => {
+  viewFile: (partialsUnready, partialsReady) => {
     function readFiles(dirname) {
       let result = [];
+
       fs.readdirSync(dirname).forEach(fileName => {
-      const contents = fs.readFileSync(dirname+'/'+fileName, 'utf-8');
-      result.push({ name: fileName, contents: contents})
-      })
+        const tempPath = dirname + '/' + fileName;
+
+        const contents = fs.readFileSync(tempPath, 'utf-8');
+
+        result.push({ name: fileName, contents: contents })
+      });
+
       return result;
     }
-    let allFiles = readFiles(partialsNotReady).concat(readFiles(partialsReady));
+
+    const tempFiles = readFiles(partialsUnready),
+    allFiles = tempFiles.concat(readFiles(partialsReady));
+
     allFiles.sort((a, b) => {
       return a.name.localeCompare(b.name, undefined, {
         numeric: true,
-        sensitivity: 'base',
+        sensitivity: 'base'
       });
     });
+
     let finalFile = '';
-    for (let file of allFiles) {
+
+    for (const file of allFiles)
       finalFile += file.contents + '\n\n';
-    }
+
     return finalFile;
   },
   parsingParagraph: (startFile, endCatalog) => {
-    let fs = require('fs');
-    fs.readFile(startFile, 'utf8', function (err, data) {
+    const fs = require('fs');
+
+    fs.readFile(startFile, 'utf8', function(err, data) {
       if (data) {
         const path = require('path');
+
         fs.mkdir(
           path.join(__dirname, endCatalog),
           { recursive: true },
           (err) => {}
         );
 
-        const paragraphText = data.split('\\n');
+        const paragraphText = data.split('\\n'),
+        resultArray = [...paragraphText];
 
-        const resultArray = [...paragraphText];
         for (let i = 0; i < paragraphText.length; i++) {
           if (paragraphText[i].match(/[!.?]/g).length > 6) {
             resultArray.splice(i, 1);
+
             const temp = paragraphText[i]
               .replace(/[.]/g, '.&')
               .replace(/[!]/g, '!&')
@@ -47,8 +60,10 @@ const fileProcessor = {
               .split('&');
 
             let result = '';
+
             for (let i = 1; i <= temp.length; i++) {
               result += temp[i - 1];
+
               if (i % 6 === 0 || i === temp.length) {
                 resultArray.push(result);
                 result = '';
@@ -56,49 +71,48 @@ const fileProcessor = {
             }
           }
         }
+
         for (let i = 0; i < resultArray.length; i++) {
           fs.writeFile(
             `${endCatalog}/part${i + 1}.txt`,
             resultArray[i],
             {
               encoding: 'utf8',
-              flag: 'w',
-              //    mode: 0o666,
+              flag: 'w'
             },
-            (err) => {
-              if (err) console.log(err);
-            }
+            (err) => { if (err) console.log(err); }
           );
         }
       }
-      if (err) {
-        console.log(err);
-      }
+
+      if (err) console.log(err);
     });
+
     return endCatalog;
   },
-  getSortedPartials: (partialsFolder) => {
-    const partials = fs.readdirSync(partialsFolder)
+  getSortedPartials: partialsFolder => {
+    const partials = fs.readdirSync(partialsFolder);
+
     partials.sort((a, b) => {
-      return a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' });    
-    })
+      return a.localeCompare(
+        b,
+        undefined,
+        { numeric: true, sensitivity: 'base' }
+      );    
+    });
+
     return partials;
   },
-  readFile: (pathToFile) => {
-    const file = fs.readFileSync(pathToFile, 'utf-8');
-    return file;
-  },
+  readFile: pathToFile => fs.readFileSync(pathToFile, 'utf-8'),
   completePart: (oldPath, newPath, contents) => {
     fs.write(oldPath, contents, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    })
+      if (err) console.log(err);
+    });
+
     fs.rename(oldPath, newPath, (err) => {
       console.log(err);
-    })
+    });
   }
 };
-
 
 module.exports = fileProcessor;
